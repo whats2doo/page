3fe145094e20fda30862b1cafeeac9e3d2d7161b
Lucas <lucas@loeffel.io>
2018-01-26 20:52:03 +0100

    test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,11 +9,10 @@ class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
-     *
      * @return void
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
 }
