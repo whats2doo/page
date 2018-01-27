9647e3b207f1800cb2f968f4e52ac8eb7007a861
Lucas <lucas@loeffel.io>
2018-01-27 19:17:17 +0100

    added env con

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import './bootstrap'
 
+window.env = document.querySelector('html').getAttribute('data-env')
+
 Vue.component(
   'example-component',
   require('./components/ExampleComponent.vue'),
@@ -9,5 +11,3 @@ Vue.component(
 const app = new Vue({
   el: '#app',
 })
-
-app()
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,6 +8,9 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ rand(1,9999) }}" media="screen" type="text/css">
 </head>
 <body>
+@if(app()->environment() !== 'prod' || app()->environment() !== 'production')
+    <div id="env">{{ app()->environment() }}</div>
+@endif
 @yield('content')
 <script type="text/javascript" src="{{ asset('js/app.js') }}?v={{ rand(1,9999) }}"></script>
 </body>
