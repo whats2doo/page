1bbf602f6bb44e3f226404807f85e8c97ff41f4d
Lucas <lucas@loeffel.io>
2018-01-27 23:00:32 +0100

    added hannes comment

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,7 @@
 @section('title', $title)
 
 @section('content')
+    Hannes stinkt von oben bis unten den ganzen tag lang
     <div id="app">
         <example-component></example-component>
         {{ __('messages.test') }}: Das ist home 2
