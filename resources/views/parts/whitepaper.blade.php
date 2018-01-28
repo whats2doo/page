ba3a775cd931f6844bda02aefebece3a9f068427
Lucas <lucas@loeffel.io>
2018-01-28 18:15:58 +0100

    updated whitepaper

--- resources/views/parts/whitepaper.blade.php
+++ resources/views/parts/whitepaper.blade.php
@@ -1,3 +1,9 @@
 <div id="whitepaper">
-    <i class="ion-md-git-branch"></i> Read our Whitepaper
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <i class="ion-md-git-branch"></i> Read our Whitepaper
+            </div>
+        </div>
+    </div>
 </div>
