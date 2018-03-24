6c65a3dbcbf8bd6c4ab8cdad4ecb12ade488162b
Lucas Löffel <lucas@loeffel.io>
2018-03-24 16:28:53 +0100

    completed

--- tests/Javascript/penis.spec.js
+++ tests/Javascript/penis.spec.js
@@ -1,11 +1,11 @@
-import { mount } from '@vue/test-utils'
 import expect from 'expect'
 import penis from '../../resources/assets/js/components/penis/penis.vue'
+import { mountLocale } from './util'
 
 describe('Penis', () => {
   it('first test', () => {
-    const wrapper = mount(penis)
+    const wrapper = mountLocale(penis)
 
-    expect(wrapper.html()).toContain('Example Component')
+    expect(wrapper.html()).toContain('Header en')
   })
 })
