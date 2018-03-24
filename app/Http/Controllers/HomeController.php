cdd92596aaad25b69ad7091d12966df47f81869f
Lucas Löffel <lucas@loeffel.io>
2018-03-24 18:23:55 +0100

    completed mission

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
+/**
+ * Class HomeController
+ * @package App\Http\Controllers
+ */
 class HomeController extends Controller
 {
-    public function home(Request $request)
+    /**
+     * @return Response
+     */
+    public function home()
     {
         return response()->view('pages.home', [
-            'title' => 'Home',
-            'menu' => config('menu'),
-            'team' => config('team'),
+            'title'    => 'Home',
+            'menu'     => config('menu'),
+            'team'     => config('team'),
             'advisors' => config('advisors')
         ]);
     }
---
+++ config/mission.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    [
+        'headline' => 'messages.mission.article1.headline',
+        'text'     => 'messages.mission.article1.text',
+        'icon'     => 'check.png'
+    ],
+
+    [
+        'headline' => 'messages.mission.article2.headline',
+        'text'     => 'messages.mission.article2.text',
+        'icon'     => 'handshake.png'
+    ],
+
+    [
+        'headline' => 'messages.mission.article3.headline',
+        'text'     => 'messages.mission.article3.text',
+        'icon'     => 'team.png'
+    ],
+
+    [
+        'headline' => 'messages.mission.article4.headline',
+        'text'     => 'messages.mission.article4.text',
+        'icon'     => 'rocket.png'
+    ],
+];
