88262411a2d2e22d43f1fddd3c37a41f9369a23a
Lucas Löffel <lucas@loeffel.io>
2018-03-24 22:54:44 +0100

    added whitepaper and link to whitepaper

--- resources/views/parts/whitepaper.blade.php
+++ resources/views/parts/whitepaper.blade.php
@@ -3,10 +3,13 @@
         <div id="window_cleaner_container">
             <div id="windowCleaner"></div>
         </div>
+
         <div class="row">
             <div class="col-xs-12">
-                <a href=""><i class="ion-ios-cloud-download-outline"></i> {{ __('messages.whitepaper.link') }}</a>
-            </div>
-        </div>
-    </div>
+                <a href="{{ asset('documents/whitepaper.pdf') }}" target="_blank">
+                    <i class="ion-ios-cloud-download-outline"></i> {{ __('messages.whitepaper.link') }}
+                </a>
+            </div><!-- col -->
+        </div><!-- row -->
+    </div><!-- container -->
 </div>
