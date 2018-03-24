1b1d915e9e971b22fd6f4cd3c5d343d76e14f3b8
Lucas Löffel <lucas@loeffel.io>
2018-03-24 22:25:00 +0100

    done

--- resources/views/parts/about.blade.php
+++ resources/views/parts/about.blade.php
@@ -12,7 +12,7 @@
                 </p>
             </div><!-- col -->
 
-            <div class="col-md-5">
+            <div class="col-lg-3 col-lg-push-2 col-lg-offset-0 col-md-4 col-md-push-1 col-md-offset-0 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3">
                 <div id="carpenter"></div>
             </div><!-- col -->
         </div><!-- row -->
