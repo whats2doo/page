9b90dfd35809aeebb4fd34f6005d5886dfc5b17a
Lucas Löffel <lucas@loeffel.io>
2018-03-25 22:47:21 +0200

    added mobile updates for the header and completed numbers

--- resources/views/parts/numbers.blade.php
+++ resources/views/parts/numbers.blade.php
@@ -1,11 +1,15 @@
 <div id="numbers">
+    <div id="gardener"></div>
+
     <div class="container">
         <div class="row">
             <div class="col-md-3 col-sm-6">
                 <div class="whats2doo-logo">
-                    logo
-                </div>
-            </div>
+                    <a href="https://whats2doo.com" target="_blank">
+                        <img src="{{ asset('images/whats2doo.svg') }}" alt="whats2doo-logo">
+                    </a>
+                </div><!-- whats2doo-logo -->
+            </div><!--col -->
 
             @if(!empty(config('numbers')))
                 @foreach(config('numbers') as $numberItem)
