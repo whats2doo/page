863a0589bc5c7ea6eb6e0312e701808728362e63
Lucas Löffel <lucas@loeffel.io>
2018-03-25 21:56:15 +0200

    added some comments

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,19 +4,42 @@
 
 @section('header')
     <header>
-        <div class="header_bg">
-            @include('parts.navbar')
-            <div id="W2D">
-                <img src="{{ asset('images/W2D.svg') }}" alt="W2D">
-            </div>
-        </div>
+        @include('parts.navbar')
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div id="w2d">
+                        <div id="painter"></div>
+                        <div id="cleaner"></div>
+                    </div><!-- w2d -->
+
+                    <div id="header-content">
+                        <h1>{{ __('messages.header.why') }}</h1>
+
+                        <div id="token-sale-timer">
+                            {!! __('messages.header.token-sale') !!}
+                        </div><!-- token-sale-timer -->
+
+                        <div id="header-buttons">
+                            @spaceless
+                            <a href="#" class="btn btn-default btn-lg">{{ __('messages.header.sign-up') }}</a>
+                            <a href="#" class="btn btn-white btn-lg">{{ __('messages.header.whitepaper') }}</a>
+                            @endspaceless
+                        </div><!-- header-buttons -->
+
+                        <h2>{{ __('messages.header.official') }}</h2>
+                    </div><!-- header-content -->
+                </div><!-- col -->
+            </div><!-- row -->
+        </div><!-- container -->
     </header>
 @endsection
 
 @section('content')
-    <div id="penis">
-        <penis></penis>
-    </div>
+    {{--<div id="penis">--}}
+    {{--<penis></penis>--}}
+    {{--</div>--}}
 
     @include('parts.about')
     @include('parts.mission')
