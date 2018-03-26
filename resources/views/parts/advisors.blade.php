0fe82c7af3ffe3de921c0dc313f53def5a41834e
Julian Daiber <julian.daiber@outlook.de>
2018-03-26 17:44:24 +0200

    removed box class

--- resources/views/parts/advisors.blade.php
+++ resources/views/parts/advisors.blade.php
@@ -1,4 +1,4 @@
-<section id="advisors" class="box">
+<section id="advisors">
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 text-center">
@@ -8,29 +8,35 @@
         </div><!-- row -->
 
         @if(!empty($advisors))
-                <div class="row text-center">
-                    <div class="list_section">
+            <div class="row text-center">
+                <div class="list_section">
                     @foreach ($advisors as $member)
                         <div class="col-sm-6 col-md-3 col-lg-3 member">
-                            <img class="img-circle teamimage lazyload" src="{{ asset('images/team/placeholder.jpg') }}" data-original="{{ asset('images/team/'.$member['image']) }}" alt="" width="160" height="160">
+                            <img class="img-circle teamimage lazyload" src="{{ asset('images/team/placeholder.jpg') }}"
+                                 data-original="{{ asset('images/team/'.$member['image']) }}" alt="" width="160"
+                                 height="160">
                             <h3>{{ $member['name'] }}</h3>
                             <div class="jobtitle">{{ $member['jobtitle'] }}</div>
                             <div class="socialprofiles">
                                 @if(!empty($member['socialprofiles']))
                                     @foreach ($member['socialprofiles'] as $profile)
                                         @if ($profile['url'] != '')
-                                            <a href="{{ $profile['url'] }}"><i class="ion-{{ $profile['icon'] }}"></i></a>
+                                            <a href="{{ $profile['url'] }}"><i
+                                                        class="ion-{{ $profile['icon'] }}"></i></a>
                                         @endif
                                     @endforeach
                                 @endif
                             </div>
                         </div>
                         @if ($loop->iteration % 4 == 0)
-                            </div></div><div class="row text-center"><div class="list_section">
-                        @endif
-                    @endforeach
-                    </div>
                 </div>
+            </div>
+            <div class="row text-center">
+                <div class="list_section">
+                    @endif
+                    @endforeach
+                </div>
+            </div>
         @endif
     </div>
 </section>
--- resources/views/parts/team.blade.php
+++ resources/views/parts/team.blade.php
@@ -1,4 +1,4 @@
-<section id="team" class="box">
+<section id="team">
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 text-center">
@@ -8,18 +8,21 @@
         </div><!-- row -->
 
         @if(!empty($team))
-                <div class="row text-center">
-                    <div class="list_section">
+            <div class="row text-center">
+                <div class="list_section">
                     @foreach ($team as $member)
                         <div class="col-sm-6 col-md-3 col-lg-3 member">
-                            <img class="img-circle teamimage lazyload" src="{{ asset('images/team/placeholder.jpg') }}" data-original="{{ asset('images/team/'.$member['image']) }}" alt="" width="160" height="160">
+                            <img class="img-circle teamimage lazyload" src="{{ asset('images/team/placeholder.jpg') }}"
+                                 data-original="{{ asset('images/team/'.$member['image']) }}" alt="" width="160"
+                                 height="160">
                             <h3>{{ $member['name'] }}</h3>
                             <div class="jobtitle">{{ $member['jobtitle'] }}</div>
                             <div class="socialprofiles">
                                 @if(!empty($member['socialprofiles']))
                                     @foreach ($member['socialprofiles'] as $profile)
                                         @if($profile['url']!='')
-                                            <a href="{{ $profile['url'] }}"><i class="ion-{{ $profile['icon'] }}"></i></a>
+                                            <a href="{{ $profile['url'] }}"><i
+                                                        class="ion-{{ $profile['icon'] }}"></i></a>
                                         @endif
                                     @endforeach
                                 @endif
@@ -30,11 +33,14 @@
 
                         </div>
                         @if ($loop->iteration % 4 == 0)
-                            </div></div><div class="row text-center"><div class="list_section">
-                        @endif
-                    @endforeach
-                    </div>
                 </div>
+            </div>
+            <div class="row text-center">
+                <div class="list_section">
+                    @endif
+                    @endforeach
+                </div>
+            </div>
         @endif
     </div>
 </section>
