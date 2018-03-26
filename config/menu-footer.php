953a3231057c2fec6240ec254cda05df80a5649e
Julian Daiber <julian.daiber@outlook.de>
2018-03-26 20:18:55 +0200

    basic design

---
+++ config/menu-footer.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    [
+        'title' => 'home',
+        'href'  => '#home',
+    ],
+
+    [
+        'title' => 'about',
+        'href'  => '#about',
+    ],
+
+    [
+        'title' => 'faq',
+        'href'  => '#faq',
+    ],
+
+    [
+        'title' => 'roadmap',
+        'href'  => '#roadmap',
+    ],
+
+    [
+        'title' => 'mission',
+        'href'  => '#mission',
+    ],
+
+    [
+        'title' => 'ico-details',
+        'href'  => '#ico-details',
+    ],
+
+    [
+        'title' => 'team',
+        'href'  => '#team',
+    ],
+
+    [
+        'title' => 'blog',
+        'href'  => '#blog',
+    ],
+];
