31db062c5a9e21b195eabce567a22b08513a0ee4
Julian Daiber <julian.daiber@outlook.de>
2018-04-05 22:52:57 +0200

    added section

---
+++ resources/views/parts/companies-header.blade.php
@@ -0,0 +1,15 @@
+@if(!empty(config('companies-header')))
+<section id="company-logos-header" class="box">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-12">
+                @foreach(config('companies-header') as $company)
+                <a href="{{ $company['url'] }}" target="_blank" title="{{ $company['title'] }}">
+                    <img src="{{ asset('images/companies/'.$company['image']) }}">
+                </a>
+                @endforeach
+            </div><!-- col -->
+        </div><!-- row -->
+    </div>
+</section>
+@endif
