c247c1c89f796749bb525fc4f8052da1d384c4a1
Julian Daiber <julian.daiber@outlook.de>
2018-04-05 22:57:18 +0200

    fixed image path

--- resources/views/parts/companies-header.blade.php
+++ resources/views/parts/companies-header.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-12">
                 @foreach(config('companies-header') as $company)
                 <a href="{{ $company['url'] }}" target="_blank" title="{{ $company['title'] }}">
-                    <img src="{{ asset('images/companies/'.$company['image']) }}">
+                    <img src="{{ asset('images/companies-header/'.$company['image']) }}">
                 </a>
                 @endforeach
             </div><!-- col -->
