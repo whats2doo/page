e003a6c3114eab5b8b140549ac78d696ac40d60a
Julian Daiber <julian.daiber@outlook.de>
2018-04-06 12:53:18 +0200

    php code optimization, vue prevent

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -15,19 +15,15 @@ class SubscribeController extends Controller
     public function sendGridConnect()
     {
         $sg = new \SendGrid(getenv('SENDGRID_API_KEY'));
-
         return $sg;
     }
 
     public function insert(Request $request)
     {
         if (!$this->validateEmail($request['email'])) {
-            $response = response()->json(null, 400);
-        } else {
-            $response = $this->getResponse($request['email']);
+            return response()->json(null, 400);
         }
-
-        return $response;
+        return  $this->getResponse($request['email']);
     }
 
     public function getResponse($email)
@@ -35,12 +31,9 @@ class SubscribeController extends Controller
         $this->sg = $this->sendGridConnect();
 
         if ($this->insertEmail($email)) {
-            $response = response()->json('success', 200);
-        } else {
-            $response = response()->json('exists', 200);
+            return response()->json('success', 200);
         }
-
-        return $response;
+        return response()->json('exists', 200);
     }
 
     public function validateEmail($email): bool
@@ -51,22 +44,32 @@ class SubscribeController extends Controller
     public function insertEmail($email): bool
     {
         $request_body = (object)['email' => $email];
-        $response = $this->sg->client->contactdb()->recipients()->post([$request_body]);
+        $response = $this
+            ->sg
+            ->client->
+            contactdb()->
+            recipients()->
+            post([$request_body]);
+
         $body = json_decode($response->body());
 
         if ($response->statusCode() == 201 && $body->new_count == 1) {
             $this->addToList($body->persisted_recipients[0]);
-
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     public function addToList($id): int
     {
-        $list_id = getenv('SENDGRID_LIST_ID');
-        $response = $this->sg->client->contactdb()->lists()->_($list_id)->recipients()->_($id)->post();
+        $response = $this
+            ->sg
+            ->client->contactdb()
+            ->lists()
+            ->_(getenv('SENDGRID_LIST_ID'))
+            ->recipients()
+            ->_($id)
+            ->post();
 
         return $response->statusCode();
     }
