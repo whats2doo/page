27364497edf7ae0b720a2f6e57538261d9695938
Julian Daiber <julian.daiber@outlook.de>
2018-04-06 19:06:23 +0200

    css changes

--- resources/views/parts/companies.blade.php
+++ resources/views/parts/companies.blade.php
@@ -15,7 +15,7 @@
                 <div class="col-md-12">
                     @foreach(config('companies') as $company)
                         <a href="{{ $company['url'] }}" target="_blank" title="{{ $company['title'] }}">
-                            <img src="{{ asset('images/companies/'.$company['image']) }}">
+                            <img src="{{ asset('images/companies/' . $company['image']) }}">
                         </a>
                     @endforeach
                 </div><!-- col -->
