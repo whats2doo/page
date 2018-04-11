1d204ea19a85a8f71f7855af625d277e1e2560d9
Julian Daiber <julian.daiber@outlook.de>
2018-04-11 17:32:33 +0200

    added subscribe animation / responsive css

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -52,9 +52,9 @@ if (document.getElementById('roadmap-app')) {
   })
 }
 
-if (document.getElementById('subscribe')) {
+if (document.getElementById('subscribe-container')) {
   Vue.create({
-    el: '#subscribe',
+    el: '#subscribe-container',
     i18n,
     components: {
       subscribe,
