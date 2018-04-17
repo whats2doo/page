3e4346c0b2139f1de101b4ac46699b29655cd922
Julian Daiber <julian.daiber@outlook.de>
2018-04-17 20:01:48 +0200

    added images and tags in master template

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,6 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
+    <link rel="apple-touch-icon-precomposed" href="{{ asset('images/apple-touch-icon-180x180.png') }}">
+    <link rel="icon" href="{{ asset('images/apple-touch-icon-180x180.png') }}">
+
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}?v={{ rand(1,9999) }}" media="screen" type="text/css">
 </head>
